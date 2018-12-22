2b5a7ee33e26576cb0fa37ab7b96db2da24c243c
markhaskins <markhaskins@mac.com>
2018-12-22 22:10:55 +0000

    script now handles an optional -p (profile) flag

--- cdk-iac.sh
+++ cdk-iac.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-c cdk] [-t template] [-a application] [-d DTAP] [-v VPC (optional)]" 1>&2; exit 1; }
+usage() {
+    echo "Usage: $0"
+    echo "Options:";
+    echo "    -c cdk          REQUIRED: CDK command to run";
+    echo "    -t cdk          REQUIRED: Template to use";
+    echo "    -a cdk          REQUIRED: Application Name";
+    echo "    -d cdk          REQUIRED: DTAP";
+    echo "    -v cdk          VPC to use";
+    echo "    -p cdk          Use DTAP as profile";
+    exit 1;
+}
 
-while getopts ":c:t:a:d:v:" o; do
+while getopts "c:t:a:d:v:p" o; do
     case "${o}" in
         c) c=${OPTARG};;
         t) t=${OPTARG};;
         a) a=${OPTARG};;
         d) d=${OPTARG};;
         v) v=${OPTARG};;
+        p) profiles=1;;
     esac
 done
 shift $((OPTIND-1))
@@ -17,14 +28,23 @@ if [[ -z ${c} ]] || [[ -z ${t} ]] || [[ -z ${a} ]] || [[ -z ${d} ]]; then
     usage
 fi
 
-JAVA_COMMAND="java -cp target/classes:$(cat .classpath.txt)"
+JAVA_COMMAND="java -cp"
+JAVA_COMMAND_FULL="java -cp target/classes:$(cat .classpath.txt)"
 
 CDK_COMMAND="${c}"
 TEMPLATE="io.haskins.cdkiac.template.${t}"
 APPLICATION="-Dapplication=${a}"
 DTAP="-Ddtap=${d}"
-VPC="-Dvpc=${v}"
 
+VPC=""
+if [[ -n ${v} ]]; then
+    VPC="-Dvpc=${v}"
+fi
 
-# exec cdk --profile ${d} --app "java -cp target/classes:$(cat .classpath.txt) io.haskins.cdkiac.template.${t} ${a} ${d} ${v}" ${c}
-exec cdk --app "${JAVA_COMMAND} ${APPLICATION} ${DTAP} ${VPC} ${TEMPLATE}" ${c}
+PROFILE=""
+if [[ ${profiles} == 1 ]]; then
+    PROFILE="--profile ${d}"
+fi
+
+echo "Running command : cdk ${PROFILE} --app \"${JAVA_COMMAND} ${APPLICATION} ${DTAP} ${VPC} ${TEMPLATE}\" ${c}"
+# exec cdk ${PROFILE} --app "${JAVA_COMMAND_FULL} ${APPLICATION} ${DTAP} ${VPC} ${TEMPLATE}" ${c}
