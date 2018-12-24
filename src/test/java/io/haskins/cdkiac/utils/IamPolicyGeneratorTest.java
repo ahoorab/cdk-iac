40ad4cb8f473279314554b15acba666ce9e2b859
mhaskins <mark.haskins@geotogether.com>
2018-12-24 10:08:29 +0000

    initial checkin

---
+++ src/test/java/io/haskins/cdkiac/utils/IamPolicyGeneratorTest.java
@@ -0,0 +1,49 @@
+package io.haskins.cdkiac.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class IamPolicyGeneratorTest {
+
+    @Test
+    public void getServiceTrustPolicy() {
+
+        String expected = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":[\"ec2.amazonaws.com\"]},\"Action\":\"sts:AssumeRole\"}]}";
+
+        ObjectNode policy = IamPolicyGenerator.getServiceTrustPolicy("ec2.amazonaws.com");
+        String jsonPolicy = policy.toString();
+
+        Assert.assertEquals(expected, jsonPolicy);
+    }
+
+    @Test
+    public void getPolicyStatement() {
+
+        String expected = "{\"Effect\":\"Allow\",\"Action\":[\"s3:List\",\"s3:Get\"],\"Resource\":[\"*\"]}";
+
+        JsonNode statement = IamPolicyGenerator.getPolicyStatement("Allow", Arrays.asList("s3:List", "s3:Get"), Collections.singletonList("*"));
+        String jsonPolicy = statement.toString();
+
+        Assert.assertEquals(expected, jsonPolicy);
+    }
+
+    @Test
+    public void getPolicyDocument() {
+
+        String expected = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"s3:List\",\"s3:Get\"],\"Resource\":[\"*\"]}]}";
+
+        JsonNode statement = IamPolicyGenerator.getPolicyStatement("Allow", Arrays.asList("s3:List", "s3:Get"), Collections.singletonList("*"));
+        ObjectNode policy = IamPolicyGenerator.getPolicyDocument(Collections.singletonList(statement));
+
+        String jsonPolicy = policy.toString();
+
+        Assert.assertEquals(expected, jsonPolicy);
+    }
+}
