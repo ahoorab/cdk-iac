620ec7f03fc686c8d2aeeb9c2ab7a48390b0a4dd
mhaskins <mark.haskins@geotogether.com>
2018-12-24 11:29:47 +0000

    restructed for command and options to be more 'bash' like

--- cdk-iac.sh
+++ cdk-iac.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0"
+    echo "Usage: $0 [Options] <cdk-command> <template> <application> <dtap> "
     echo "Options:";
-    echo "    -c cdk          REQUIRED: CDK command to run";
-    echo "    -t cdk          REQUIRED: Template to use";
-    echo "    -a cdk          REQUIRED: Application Name";
-    echo "    -d cdk          REQUIRED: DTAP";
-    echo "    -v cdk          VPC to use";
-    echo "    -p cdk          Use DTAP as profile";
+    echo "  -v  VPC";
+    echo "  -p  Use DTAP as profile";
+    echo "";
     exit 1;
 }
 
-while getopts "c:t:a:d:v:p" o; do
+while getopts ":v:p" o; do
     case "${o}" in
-        c) c=${OPTARG};;
-        t) t=${OPTARG};;
-        a) a=${OPTARG};;
-        d) d=${OPTARG};;
         v) v=${OPTARG};;
         p) profiles=1;;
     esac
 done
 shift $((OPTIND-1))
 
-if [[ -z ${c} ]] || [[ -z ${t} ]] || [[ -z ${a} ]] || [[ -z ${d} ]]; then
+if [[ -z ${1} ]] || [[ -z ${2} ]] || [[ -z ${3} ]] || [[ -z ${4} ]]; then
     usage
 fi
 
+CDK_COMMAND="$1"
+TEMPLATE="io.haskins.cdkiac.template.$2"
+APPLICATION="-Dapplication=$3"
+DTAP="-Ddtap=$4 "
+
 JAVA_COMMAND="java -cp"
 JAVA_COMMAND_FULL="java -cp target/classes:$(cat .classpath.txt)"
-
-CDK_COMMAND="${c}"
-TEMPLATE="io.haskins.cdkiac.template.${t}"
-APPLICATION="-Dapplication=${a} " # space at the end needed
-DTAP="-Ddtap=${d} " # space at the end needed
 
 VPC=""
 if [[ -n ${v} ]]; then
@@ -43,8 +36,8 @@ fi
 
 PROFILE=""
 if [[ ${profiles} == 1 ]]; then
-    PROFILE="--profile ${d}"
+    PROFILE="--profile $4"
 fi
 
-echo "Running command : cdk ${PROFILE} --app \"${JAVA_COMMAND} ${APPLICATION}${DTAP}${VPC}${TEMPLATE}\" ${c}"
-exec cdk ${PROFILE} --app "${JAVA_COMMAND_FULL} ${APPLICATION}${DTAP}${VPC}${TEMPLATE}" ${c}
+echo "Running command : cdk ${PROFILE} --app \"${JAVA_COMMAND} ${APPLICATION} ${DTAP}${VPC}${TEMPLATE}\" ${CDK_COMMAND}"
+exec cdk ${PROFILE} --app "${JAVA_COMMAND_FULL} ${APPLICATION} ${DTAP}${VPC}${TEMPLATE}" ${CDK_COMMAND}
